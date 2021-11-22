5f2a526aa58f72c3df85992443f0eafbcb51547c
Duckwrth <charles.olagunju@ontariotechu.net>
2021-11-21 21:53:12 -0500

    question 1

---
+++ Question 1/ConsoleApp/src/main/java/com/mycompany/ConsoleApp.java
@@ -0,0 +1,8 @@
+package com.mycompany;
+
+public class ConsoleApp {
+    public static void main(String[] args){
+        String appName = commonUtility.getAppName();
+        System.out.println("Welcome to " + appName);
+    }
+}
---
+++ Question 1/DesktopApp/src/main/java/com/mycompany/commonUtility.java
@@ -0,0 +1,7 @@
+package com.mycompany;
+
+public class commonUtility {
+    public static String getAppName(){
+        return "My Company Project Beta Version";
+    }
+}
